cce53b28ae0758bac94d6aeb368355dd7deb1434
Aanchal-kumari-R <aanchalkumari096@gmail.com>
2024-09-05 15:23:34 +0530

    Adding project template

--- resume/views.py
+++ resume/views.py
@@ -5,3 +5,38 @@ from django.shortcuts import render
 
 def home(request): 
     return render(request,"home.html")
+
+def about(request): 
+    return render(request,"about.html")
+
+def projects(request):  
+    projects_show = [ 
+        { 
+            "title":"Crud Operation", 
+            "path": "images/Crud.PNG"
+        }, 
+
+        { 
+            "title":"Ecommerce Website", 
+            "path":"images/ecom_website.PNG" 
+        },  
+
+        {  
+           "title":"Portfolio", 
+           "path":"images/portfolio.PNG"
+        }, 
+
+        { 
+            "title":"API Based Ecommerce", 
+            "path":"images/ecomsite.PNG"
+        }  
+        
+
+    ]
+    return render(request,"projects.html",{"projects_show":projects_show}) 
+
+def experience(request):  
+  
+
+    return render(request,"experience.html")
+
